ac102de426c8765c79da4ef628481d02e1d324a3
Jorge H <jorgehenrrique@live.com>
2023-03-10 18:09:35 -0300

    rmv comentarios

--- assets/js/main.js
+++ assets/js/main.js
@@ -104,18 +104,6 @@ function limpaForm(){ // Limpa todos formularios
     }
 }
 
-// function confirmaExcluir(){ // Confirmar excluir em Categorias
-//     console.log('Excluir?')
-//     confirmarExcluir.classList.remove('none');
-//     document.querySelector('.simExcluir').addEventListener('click', () => {
-//         console.log('Deletado');
-//         confirmarExcluir.classList.add('none');
-//     });
-//     document.querySelector('.naoExcluir').addEventListener('click', () => {
-//         console.log('Não deletado');
-//         confirmarExcluir.classList.add('none');
-//     });
-// }
 
 // --------------------------------------------------------
 
